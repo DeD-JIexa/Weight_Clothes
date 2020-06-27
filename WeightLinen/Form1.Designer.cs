ef721e2b318c7df07369a24c806997d544a45060
Alex <alex3kot@outlook.com>
2020-06-27 17:35:27 +0400

    Добавьте файлы проекта.

---
+++ WeightLinen/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace WeightLinen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Fytbolka = new System.Windows.Forms.TextBox();
+            this.Pybashka = new System.Windows.Forms.TextBox();
+            this.PlatieKorotkoe = new System.Windows.Forms.TextBox();
+            this.PlatieDlinnoe = new System.Windows.Forms.TextBox();
+            this.Noski = new System.Windows.Forms.TextBox();
+            this.Kolgotki = new System.Windows.Forms.TextBox();
+            this.Prostinia = new System.Windows.Forms.TextBox();
+            this.Pododialnik = new System.Windows.Forms.TextBox();
+            this.PolotenceMalenkoe = new System.Windows.Forms.TextBox();
+            this.PolotenceBolshoe = new System.Windows.Forms.TextBox();
+            this.Skatert = new System.Windows.Forms.TextBox();
+            this.Navolochka = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.Srart = new System.Windows.Forms.Button();
+            this.FinishText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Fytbolka
+            // 
+            this.Fytbolka.Location = new System.Drawing.Point(109, 49);
+            this.Fytbolka.Name = "Fytbolka";
+            this.Fytbolka.Size = new System.Drawing.Size(86, 20);
+            this.Fytbolka.TabIndex = 0;
+            this.Fytbolka.TextChanged += new System.EventHandler(this.Fytbolka_TextChanged);
+            // 
+            // Pybashka
+            // 
+            this.Pybashka.Location = new System.Drawing.Point(109, 75);
+            this.Pybashka.Name = "Pybashka";
+            this.Pybashka.Size = new System.Drawing.Size(86, 20);
+            this.Pybashka.TabIndex = 1;
+            this.Pybashka.TextChanged += new System.EventHandler(this.Pybashka_TextChanged);
+            // 
+            // PlatieKorotkoe
+            // 
+            this.PlatieKorotkoe.Location = new System.Drawing.Point(109, 97);
+            this.PlatieKorotkoe.Name = "PlatieKorotkoe";
+            this.PlatieKorotkoe.Size = new System.Drawing.Size(85, 20);
+            this.PlatieKorotkoe.TabIndex = 2;
+            this.PlatieKorotkoe.TextChanged += new System.EventHandler(this.PlatieKorotkoe_TextChanged);
+            // 
+            // PlatieDlinnoe
+            // 
+            this.PlatieDlinnoe.Location = new System.Drawing.Point(110, 123);
+            this.PlatieDlinnoe.Name = "PlatieDlinnoe";
+            this.PlatieDlinnoe.Size = new System.Drawing.Size(85, 20);
+            this.PlatieDlinnoe.TabIndex = 3;
+            this.PlatieDlinnoe.TextChanged += new System.EventHandler(this.PlatieDlinnoe_TextChanged);
+            // 
+            // Noski
+            // 
+            this.Noski.Location = new System.Drawing.Point(110, 149);
+            this.Noski.Name = "Noski";
+            this.Noski.Size = new System.Drawing.Size(85, 20);
+            this.Noski.TabIndex = 4;
+            this.Noski.TextChanged += new System.EventHandler(this.Noski_TextChanged);
+            // 
+            // Kolgotki
+            // 
+            this.Kolgotki.Location = new System.Drawing.Point(108, 175);
+            this.Kolgotki.Name = "Kolgotki";
+            this.Kolgotki.Size = new System.Drawing.Size(86, 20);
+            this.Kolgotki.TabIndex = 5;
+            this.Kolgotki.TextChanged += new System.EventHandler(this.Kolgotki_TextChanged);
+            // 
+            // Prostinia
+            // 
+            this.Prostinia.Location = new System.Drawing.Point(108, 201);
+            this.Prostinia.Name = "Prostinia";
+            this.Prostinia.Size = new System.Drawing.Size(86, 20);
+            this.Prostinia.TabIndex = 6;
+            this.Prostinia.TextChanged += new System.EventHandler(this.Prostinia_TextChanged);
+            // 
+            // Pododialnik
+            // 
+            this.Pododialnik.Location = new System.Drawing.Point(110, 227);
+            this.Pododialnik.Name = "Pododialnik";
+            this.Pododialnik.Size = new System.Drawing.Size(85, 20);
+            this.Pododialnik.TabIndex = 7;
+            this.Pododialnik.TextChanged += new System.EventHandler(this.Pododialnik_TextChanged);
+            // 
+            // PolotenceMalenkoe
+            // 
+            this.PolotenceMalenkoe.Location = new System.Drawing.Point(108, 253);
+            this.PolotenceMalenkoe.Name = "PolotenceMalenkoe";
+            this.PolotenceMalenkoe.Size = new System.Drawing.Size(86, 20);
+            this.PolotenceMalenkoe.TabIndex = 8;
+            this.PolotenceMalenkoe.TextChanged += new System.EventHandler(this.PolotenceMalenkoe_TextChanged);
+            // 
+            // PolotenceBolshoe
+            // 
+            this.PolotenceBolshoe.Location = new System.Drawing.Point(109, 279);
+            this.PolotenceBolshoe.Name = "PolotenceBolshoe";
+            this.PolotenceBolshoe.Size = new System.Drawing.Size(86, 20);
+            this.PolotenceBolshoe.TabIndex = 9;
+            this.PolotenceBolshoe.TextChanged += new System.EventHandler(this.PolotenceBolshoe_TextChanged);
+            // 
+            // Skatert
+            // 
+            this.Skatert.Location = new System.Drawing.Point(108, 309);
+            this.Skatert.Name = "Skatert";
+            this.Skatert.Size = new System.Drawing.Size(86, 20);
+            this.Skatert.TabIndex = 10;
+            this.Skatert.TextChanged += new System.EventHandler(this.Skatert_TextChanged);
+            // 
+            // Navolochka
+            // 
+            this.Navolochka.Location = new System.Drawing.Point(108, 335);
+            this.Navolochka.Name = "Navolochka";
+            this.Navolochka.Size = new System.Drawing.Size(86, 20);
+            this.Navolochka.TabIndex = 11;
+            this.Navolochka.TextChanged += new System.EventHandler(this.Navolochka_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Футболка";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Рубашка м.";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Платье к";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Платье д";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(64, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Носки";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Колготки";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(45, 204);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Простыня";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(22, 230);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Пододеяльник";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(29, 256);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(73, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Полотенце м";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(32, 282);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(71, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Полотенце б";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(49, 312);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(54, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "Скатерть";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(40, 338);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(62, 13);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "Наволочка";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // Srart
+            // 
+            this.Srart.Location = new System.Drawing.Point(257, 49);
+            this.Srart.Name = "Srart";
+            this.Srart.Size = new System.Drawing.Size(120, 46);
+            this.Srart.TabIndex = 24;
+            this.Srart.Text = "Вычислить вес";
+            this.Srart.UseVisualStyleBackColor = true;
+            this.Srart.Click += new System.EventHandler(this.Srart_Click);
+            // 
+            // FinishText
+            // 
+            this.FinishText.AutoSize = true;
+            this.FinishText.Location = new System.Drawing.Point(254, 107);
+            this.FinishText.Name = "FinishText";
+            this.FinishText.Size = new System.Drawing.Size(62, 13);
+            this.FinishText.TabIndex = 26;
+            this.FinishText.Text = "Результат:";
+            this.FinishText.Click += new System.EventHandler(this.FinishText_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FinishText);
+            this.Controls.Add(this.Srart);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Navolochka);
+            this.Controls.Add(this.Skatert);
+            this.Controls.Add(this.PolotenceBolshoe);
+            this.Controls.Add(this.PolotenceMalenkoe);
+            this.Controls.Add(this.Pododialnik);
+            this.Controls.Add(this.Prostinia);
+            this.Controls.Add(this.Kolgotki);
+            this.Controls.Add(this.Noski);
+            this.Controls.Add(this.PlatieDlinnoe);
+            this.Controls.Add(this.PlatieKorotkoe);
+            this.Controls.Add(this.Pybashka);
+            this.Controls.Add(this.Fytbolka);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Fytbolka;
+        private System.Windows.Forms.TextBox Pybashka;
+        private System.Windows.Forms.TextBox PlatieKorotkoe;
+        private System.Windows.Forms.TextBox PlatieDlinnoe;
+        private System.Windows.Forms.TextBox Noski;
+        private System.Windows.Forms.TextBox Kolgotki;
+        private System.Windows.Forms.TextBox Prostinia;
+        private System.Windows.Forms.TextBox Pododialnik;
+        private System.Windows.Forms.TextBox PolotenceMalenkoe;
+        private System.Windows.Forms.TextBox PolotenceBolshoe;
+        private System.Windows.Forms.TextBox Skatert;
+        private System.Windows.Forms.TextBox Navolochka;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button Srart;
+        private System.Windows.Forms.Label FinishText;
+    }
+}
+
---
+++ WeightLinen/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeightLinen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            #region Defolt
+            Fytbolka.Text = "0";
+            Pybashka.Text = "0";
+            PlatieKorotkoe.Text = "0";
+            PlatieDlinnoe.Text = "0";
+            Noski.Text = "0";
+            Kolgotki.Text = "0";
+            Prostinia.Text = "0";
+            Pododialnik.Text = "0";
+            PolotenceMalenkoe.Text = "0";
+            PolotenceBolshoe.Text = "0";
+            Skatert.Text = "0";
+            Navolochka.Text = "0";
+            #endregion
+        }
+        #region Object
+        private void Fytbolka_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Pybashka_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlatieKorotkoe_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlatieDlinnoe_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Noski_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Kolgotki_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Prostinia_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Pododialnik_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PolotenceMalenkoe_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PolotenceBolshoe_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Skatert_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Navolochka_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+        #region TextObject
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        private void FinishText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Srart_Click(object sender, EventArgs e)
+        {
+            #region ObjectNum
+
+            int fytbolka = Convert.ToInt32(Fytbolka.Text);
+            int fyt = fytbolka * 100;
+
+            int pybashka = Convert.ToInt32(Pybashka.Text);
+            int pyb = pybashka * 150;
+            
+            int platiekorotkoe = Convert.ToInt32(PlatieKorotkoe.Text);
+            int plak = platiekorotkoe * 50;
+
+            int platiedlinnoe = Convert.ToInt32(PlatieDlinnoe.Text);
+            int plad = platiedlinnoe * 100;
+
+            int noski = Convert.ToInt32(Noski.Text);
+            int nosk = noski * 10;
+            
+            int kolgotki = Convert.ToInt32(Kolgotki.Text);
+            int kolg = kolgotki * 30;
+
+            int prostinia = Convert.ToInt32(Prostinia.Text);
+            int prost = prostinia * 200;
+
+            int pododialnik = Convert.ToInt32(Pododialnik.Text);
+            int podo = pododialnik * 400;
+
+            int polotencemalenkoe = Convert.ToInt32(PolotenceMalenkoe.Text);
+            int polom = polotencemalenkoe * 100;
+
+            int polotencebolshoe = Convert.ToInt32(PolotenceBolshoe.Text);
+            int polob = polotencebolshoe * 200;
+            
+            int skatert = Convert.ToInt32(Skatert.Text);
+            int skat = skatert * 500;
+
+            int navolochka = Convert.ToInt32(Navolochka.Text);
+            int navo = navolochka * 150;
+            #endregion
+
+            int rez = fyt + pyb + plak + plad + nosk + kolg + prost + podo + polom + polob + skat + navo;
+
+            FinishText.Text = "Результат: " + rez.ToString() + " гр.";
+        }
+    }
+}
